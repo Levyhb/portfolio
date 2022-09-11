edd64ae3a74882610cb00e9fc488f6d6b6eb1ece
Levy Bezerra <levybholanda@gmail.com>
2022-09-11 10:20:48 -0300

    Criado o Navbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Routes >
+        <Route path='/' element={ <Home />}/>
+        <Route path='/contact' element={ <Contact />}/>
+        <Route path='/projects' element={ <Projects />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/components/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-links">
+        <NavLink to="/" activeClassName="active" className="nav-item">
+          &lt; /&gt;
+        </NavLink>
+      </div>
+      <div className="nav-links">
+        <NavLink to="/" activeClassName="active" className="nav-item">
+          Sobre mim{" "}
+        </NavLink>
+
+        <NavLink
+          to="/projects"
+          activeClassName="active"
+          className="nav-item"
+        >
+          Projetos
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          activeClassName="active"
+          className="nav-item"
+        >
+          Contato
+        </NavLink>
+
+      </div>
+    </nav>
+  );
+}
