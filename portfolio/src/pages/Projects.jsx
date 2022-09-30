ae75d8942dc0021d9cdfcbdbcbee5f8917d616a6
Levy Bezerra <levybholanda@gmail.com>
2022-09-30 16:22:31 -0300

    Adicionando título na página de projetos

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { BiArrowToTop } from "react-icons/bi";
+import { TypeAnimation } from "react-type-animation";
 import ProjectCard from "../components/ProjectCard";
 import { PROJECTS } from "../projects/project";
 import "../styles/pages/Projects.css";
 
 export default function Projects() {
   return (
-    <div className="project-container">
-      {PROJECTS.map((project) => (
-        <ProjectCard
-          key={project.id}
-          id={project.id}
-          name={project.name}
-          image={project.image}
-          gif={project.gif}
-          link={project.deploy}
-          repository={project.repository}
+    <div>
+      <div className="projects-title-container">
+        <h2 className="title-projects">
+          <span className="type-animation-projects">&lt;</span> Veja alguns dos
+        </h2>
+        <TypeAnimation
+          className="type-animation-projects"
+          sequence={[800, "Principais projetos que realizei />"]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
         />
-      ))}
-      <h2 to="/" onClick={() => window.scroll({ top: 0, behavior: "smooth" })} className="back-to-top">
-        Voltar ao topo <BiArrowToTop />{" "}
-      </h2>
+      </div>
+      <div className="project-container">
+        {PROJECTS.map((project) => (
+          <ProjectCard
+            key={project.id}
+            id={project.id}
+            name={project.name}
+            image={project.image}
+            gif={project.gif}
+            link={project.deploy}
+            repository={project.repository}
+          />
+        ))}
+        <h2
+          to="/"
+          onClick={() => window.scroll({ top: 0, behavior: "smooth" })}
+          className="back-to-top"
+        >
+          Voltar ao topo <BiArrowToTop />{" "}
+        </h2>
+      </div>
     </div>
   );
 }
